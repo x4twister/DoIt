4ceae5f1fdfe68a7e562c6fa37d1b848352203df
x4twister <57191932+x4twister@users.noreply.github.com>
2021-05-18 19:46:40 +0300

    target list binding

--- app/src/main/java/ru/x4twister/doit/DoListFragment.kt
+++ app/src/main/java/ru/x4twister/doit/DoListFragment.kt
@@ -9,7 +9,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.x4twister.doit.databinding.FragmentDoListBinding
+import ru.x4twister.doit.databinding.ListItemTargetBinding
 
 class DoListFragment: Fragment() {
 
@@ -18,10 +23,46 @@ class DoListFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_do_list,container,false)
+        val binding:FragmentDoListBinding=DataBindingUtil
+            .inflate(inflater,R.layout.fragment_do_list,container,false)
+
+        binding.recycleView.run {
+            layoutManager=LinearLayoutManager(activity)
+            adapter=TargetAdapter(listOf(Target("abc"),Target("xyz")))
+        }
+
+        return binding.root
     }
 
     companion object {
         fun newInstance()=DoListFragment()
+    }
+
+    inner class TargetHolder(val binding: ListItemTargetBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(target: Target) {
+            binding.viewModel!!.target=target
+        }
+
+        init {
+            binding.viewModel=TargetViewModel()
+        }
+    }
+
+    inner class TargetAdapter(private val targets: List<Target>): RecyclerView.Adapter<TargetHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TargetHolder {
+            val inflater=LayoutInflater.from(activity)
+            val binding:ListItemTargetBinding=DataBindingUtil
+                .inflate(inflater,R.layout.list_item_target,parent,false)
+
+            return TargetHolder(binding)
+        }
+
+        override fun getItemCount()=targets.size
+
+        override fun onBindViewHolder(holder: TargetHolder, position: Int) {
+            holder.bind(targets[position])
+        }
     }
 }
